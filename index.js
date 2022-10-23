dc7fad70849858f5ccbd6ddd38bc051eb482fbd7
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-23 21:25:21 +0530

    button and input selected

---
+++ index.js
@@ -0,0 +1,5 @@
+const inputArea = document.querySelector("#input");
+const checkButton = document.querySelector("#btn");
+const outputArea = document.querySelector("#output");
+
+checkButton.addEventListener('click',)
